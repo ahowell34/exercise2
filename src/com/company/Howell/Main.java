4eb7653931fb041b32446aaaadd4c0578eeb45bf
026654 <ahowell34@student.cscc.edu>
2020-01-21 15:08:39 -0500

    Pushing up comment and weekday

--- src/com/company/Howell/Main.java
+++ src/com/company/Howell/Main.java
@@ -1,16 +1,16 @@
+//Alysyn Howell 1/21/20 Prog Fundamentals for Android
 package com.company.Howell;
-
-import com.sun.org.apache.xpath.internal.SourceTree;
 
 public class Main {
 
     public static void main(String[] args) {
+        String[] week = {"Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
         int temp[] = {45, 29, 10, 22, 41, 28, 33};
         int rain[] = {95, 60, 25, 5, 0, 75,90};
 
         for(int j = 0; j < temp.length; j++){
             if (temp[j] <= 32 && rain[j] > 50){
-                System.out.println(temp[j] + " " + rain[j]);
+                System.out.println(temp[j] + " " + rain[j] + " " + week[j]);
             }
         }
     }
